b418fa933798e0b75da7e276993390ae20575bde
Dan Peddle <dan.peddle@gmail.com>
2013-09-08 23:47:47 +0200

    fun and totally bugged year separators

--- js/views/release-list-view.js
+++ js/views/release-list-view.js
@@ -5,13 +5,19 @@ var ReleaseListView = Backbone.View.extend({
 	tagName: 'div',
 	className: 'release-holder',
 	initialize: function(){
-		
+
 		this.collection.on('sort',this.render.bind(this));
 	},
 	render: function(){
 		
 		this.$el.empty().addClass('hidden');
+		var currentYear = false;
 		this.collection.each(function(release){
+			releaseyear = release.get('momented').year();
+			if (releaseyear !== currentYear) {
+				currentYear = releaseyear;
+				this.$el.append('<h1 class="year-sep">'+currentYear+'</h1>');
+			};
 			var releaseView = new ReleaseView({
 				model: release
 			});
