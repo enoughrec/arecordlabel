952a81c9641b438fa913e0d44820cd0a5bbc94e2
Dan Peddle <dan.peddle@gmail.com>
2013-09-09 03:07:03 +0200

    hide inactive years

--- js/views/release-list-view.js
+++ js/views/release-list-view.js
@@ -5,19 +5,25 @@ var ReleaseListView = Backbone.View.extend({
 	tagName: 'div',
 	className: 'release-holder',
 	initialize: function(){
-
 		this.collection.on('sort',this.render.bind(this));
 	},
 	render: function(){
 		
 		this.$el.empty().addClass('hidden');
 		var currentYear = false;
+		var yearVisible = false;
 		this.collection.each(function(release){
 			releaseyear = release.get('momented').year();
 			if (releaseyear !== currentYear) {
+				yearVisible = false;
 				currentYear = releaseyear;
-				this.$el.append('<h1 class="year-sep">'+currentYear+'</h1>');
+				this.$el.append('<h1 class="year-sep hidden" year="'+currentYear+'">'+currentYear+'</h1>');
 			};
+			if (!yearVisible && release.get('visible') === true) {
+				yearVisible = true;
+				this.$el.find('h1[year='+currentYear+']').removeClass('hidden');
+			};
+
 			var releaseView = new ReleaseView({
 				model: release
 			});
