1d417e126e89bf12c6bec4160e3820b85624b24b
Dan Peddle <dan.peddle@gmail.com>
2014-06-27 16:56:12 +0200

    create components for top and bottom bar, and include them

---
+++ js/components/bottombar.js
@@ -0,0 +1,14 @@
+/**
+ * @jsx React.DOM
+ */
+
+
+var React = require('react');
+
+var Bottombar = React.createClass({
+	render: function(){
+		return (<div className="bottom-bar" />);
+	}
+})
+
+module.exports = Bottombar;
