64cece9729c49d96fc0c2c1e1873b832eb648705
Dan Peddle <dan.peddle@gmail.com>
2014-06-27 18:23:20 +0200

    make links on top bar

--- js/components/topbar.js
+++ js/components/topbar.js
@@ -4,10 +4,36 @@
 
 
 var React = require('react');
+var Router = require('react-router-component');
+
+var Locations = Router.Locations;
+var Location = Router.Location;
+var Link = Router.Link;
 
 var Topbar = React.createClass({
 	render: function(){
-		return (<div className="top-bar" />);
+		var links = [{
+			link:'/',
+			label:'Enough Records'
+		}, {
+			link: '/about',
+			label: 'About'
+		}];
+
+		var comps = links.map(function(link){
+			return (
+				<span>
+					<Link href={link.link } hash>{link.label}</Link>
+				</span>
+			)
+		});
+		comps.push(<span><a href="http://enoughrecords.org">Blog</a></span>);
+
+		return (
+			<div className="top-bar">
+				{comps}
+			</div>
+		);
 	}
 })
 
