3a1b090b27ef206b86fc530d4e6a805619d9bb5f
Dan Peddle <dan.peddle@gmail.com>
2014-06-28 21:50:47 +0200

    add bottom control placeholders back

--- js/components/bottombar.js
+++ js/components/bottombar.js
@@ -7,7 +7,16 @@ var React = require('react');
 
 var Bottombar = React.createClass({
 	render: function(){
-		return (<div className="bottom-bar" />);
+		return (
+			<div className="bottom-bar">
+			<span className="controls">
+	            <span ctrl="back" className="fa fontawesome-backward"></span>
+	            <span ctrl="stop" className="fa fontawesome-stop"></span>
+	            <span id="play-state" ctrl="play" className="fa fontawesome-play"></span>
+	            <span ctrl="forward" className="fa fontawesome-forward"></span>
+	        </span>
+			</div>
+		);
 	}
 })
 
