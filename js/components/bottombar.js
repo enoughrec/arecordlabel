9dbc07c9a821bf91634fe2e08ea2e0deba732d88
Dan Peddle <dan.peddle@gmail.com>
2014-06-29 12:37:44 +0200

    fix full width player, remove unnecessary binding

--- js/components/bottombar.js
+++ js/components/bottombar.js
@@ -13,7 +13,7 @@ var Bottombar = React.createClass({
 			<div className="bottom-bar">
 				<Player />
 		        <span className="flattr">
-		        	<iframe src="http://api.flattr.com/button/view/?button=compact&amp;uid=0&amp;url=http%3A%2F%2Fenoughrecords.scene.org&amp;language=&amp;hidden=0&amp;title=&amp;category=&amp;tags=&amp;description=" width="110" height="20" frameborder="0" scrolling="no" border="0" marginheight="0" marginwidth="0" allowtransparency="true"></iframe>
+		        	<iframe src="http://api.flattr.com/button/view/?button=compact&amp;uid=0&amp;url=http%3A%2F%2Fenoughrecords.scene.org&amp;language=&amp;hidden=0&amp;title=&amp;category=&amp;tags=&amp;description=" width="110" height="20" frameBorder="0" scrolling="no" border="0" marginHeight="0" marginWidth="0" allowTransparency="true"></iframe>
 	        	</span>
 			</div>
 		);
