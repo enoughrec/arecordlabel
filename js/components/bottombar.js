fde4e9554d0567566bb1eb0a2d2a08ec567a0540
Dan Peddle <dan.peddle@gmail.com>
2014-06-29 12:24:02 +0200

    listen to queue events and start playing stuff when told to, updating the UI

--- js/components/bottombar.js
+++ js/components/bottombar.js
@@ -4,17 +4,17 @@
 
 
 var React = require('react');
+var Player = require('./player');
+
 
 var Bottombar = React.createClass({
 	render: function(){
 		return (
 			<div className="bottom-bar">
-			<span className="controls">
-	            <span ctrl="back" className="fa fontawesome-backward"></span>
-	            <span ctrl="stop" className="fa fontawesome-stop"></span>
-	            <span id="play-state" ctrl="play" className="fa fontawesome-play"></span>
-	            <span ctrl="forward" className="fa fontawesome-forward"></span>
-	        </span>
+				<Player />
+		        <span className="flattr">
+		        	<iframe src="http://api.flattr.com/button/view/?button=compact&amp;uid=0&amp;url=http%3A%2F%2Fenoughrecords.scene.org&amp;language=&amp;hidden=0&amp;title=&amp;category=&amp;tags=&amp;description=" width="110" height="20" frameborder="0" scrolling="no" border="0" marginheight="0" marginwidth="0" allowtransparency="true"></iframe>
+	        	</span>
 			</div>
 		);
 	}
