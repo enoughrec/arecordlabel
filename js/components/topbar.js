6c1852fbdbf22cc4eaf24ffb5907c64a1a2844c3
Dan Peddle <dan.peddle@gmail.com>
2015-01-22 00:29:05 +0100

    cleanup topbar

--- js/components/topbar.js
+++ js/components/topbar.js
@@ -13,33 +13,34 @@ var Link = Router.Link;
 var Searchbox = require('./searchbox');
 
 var Topbar = React.createClass({
-	render: function(){
-		var links = [{
-			link:'/',
-			label:'Enough Records'
-		}, {
-			link: '/about',
-			label: 'About'
-		}];
+    render: function(){
+        
+        var links = [{
+            link:'/',
+            label:'Enough Records'
+        }, {
+            link: '/about',
+            label: 'About'
+        }];
 
-		var comps = links.map(function(link){
-			return (
-				<span key={link.label}>
-					<Link to={link.link }>{link.label}</Link>
-				</span>
-			)
-		});
-		comps.push(<span key={"bloglink"}><a target="_blank" href="https://web.archive.org/web/20141217092309/http://enoughrecords.org/">Blog</a></span>);
+        var comps = links.map(function(link){
+            return (
+                <span key={link.label}>
+                    <Link to={link.link }>{link.label}</Link>
+                </span>
+            )
+        });
+        comps.push(<span key={"bloglink"}><a target="_blank" href="https://web.archive.org/web/20141217092309/http://enoughrecords.org/">Blog</a></span>);
 
-		return (
-			<div className="top-bar">
-				{comps}
-				<div className="right">
-					<Searchbox data={this.props.data} searchData={this.props.searchData}/>
-				</div>
-			</div>
-		);
-	}
+        return (
+            <div className="top-bar">
+                {comps}
+                <div className="right">
+                    <Searchbox data={this.props.data} searchData={this.props.searchData}/>
+                </div>
+            </div>
+        );
+    }
 })
 
 module.exports = Topbar;
