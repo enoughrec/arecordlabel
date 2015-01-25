94b0fba009e1abc1146ec7737bfab47e9b625fd2
Dan Peddle <dan.peddle@gmail.com>
2015-01-25 23:36:08 +0100

    rotated dates on listing page, same as on article

--- js/components/article-list.js
+++ js/components/article-list.js
@@ -11,17 +11,24 @@ var ArticleList = React.createClass({
     renderTitles:function(){
         var articles = this.props.articles;
         var comps = articles.map(function(article){
+
+
             var json = article.toJSON();
             var bgStyle = {
                 backgroundImage: 'url('+json.image+')'
             };
+
+            var date = article.get('date');
+            var year = date.format('YYYY');
+            var dayMonth = date.format('MMMM Do');
+            var dateString = [dayMonth, (<br/>), year];
 
             return (
                 <div className="list-article">
                     <Link to={'/blog/' + json.slug} style={bgStyle} className="image-link"></Link>
                     <div className="right-content">
                         <Link to={'/blog/' + json.slug}><h1 className="title">{json.title}</h1></Link>
-                        <div className="date">{article.get('date').format('YYYY-MM-DD')}</div>
+                        <div className="date">{dateString}</div>
                         <div className="reading-time">{json.readingTime.text}</div>
                         <div className="description">{json.description}</div>
                     </div>
