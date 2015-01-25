24748e61a35c3f7f1f0b9ff17062de808fdf7534
Dan Peddle <dan.peddle@gmail.com>
2015-01-25 02:01:26 +0100

    article styles, reading time

--- js/components/article.js
+++ js/components/article.js
@@ -1,33 +1,36 @@
 /**
- * @jsx React.DOM
- */
+* @jsx React.DOM
+*/
 
- /**
-  * @jsx React.DOM
-  */
- 
- var React = require('react');
- var Router = require('react-router');
- var Link = Router.Link;
+/**
+* @jsx React.DOM
+*/
 
- var Article = React.createClass({
+var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
+var readingTime = require('reading-time');
 
-
+var Article = React.createClass({
     render: function() {
 
         var json    = this.props.data.toJSON(),
-            article = this.props.data;
+            article = this.props.data, 
+            time = readingTime(json.body);
 
         return (
             <div className="release-full article">
                 <Link to="/blog">back</Link>
-                <div className="article-title">{json.title}</div>
-                <div className="article-date">{article.get('date').format('YYYY-MM-DD')}</div>
-                <div className="post" dangerouslySetInnerHTML={{__html: json.body}}></div>
+                <div className="centered-article">
+                    <h1 className="article-title">{json.title}</h1>
+                    <div className="article-date">{article.get('date').format('YYYY-MM-DD')}</div>
+                    <div>{time.text}</div>
+                    <div className="post" dangerouslySetInnerHTML={{__html: json.body}}></div>
+                </div>
             </div>
         );
     }
- 
- });
- 
- module.exports = Article;
+
+});
+
+module.exports = Article;
