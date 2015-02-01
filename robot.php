8fdd303918d6da7a656a8830af29c3acf7770bdb
Dan Peddle <dan.peddle@gmail.com>
2015-02-01 17:40:06 +0100

    enable sharing of release URLs via Facebook with proper meta info.

---
+++ robot.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+    <head>
+        <title><?php echo $data['album'].' - '.$data['artist'] ?></title>
+        <meta name="description" content="<?php echo $data['cat'].' - '.$data['info_en'] ?>" />
+        <meta property="og:title" content="<?php echo $data['album'] ?>" />
+        <meta property="og:description" content="<?php echo $data['info_en'] ?>" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="<?php echo $data['url'] ?>" />
+        <meta property="og:image" content="<?php echo $data['cover'] ?>" />
+        <link rel="image_src" href="<?php echo $data['cover'] ?>">
+    </head>
+    <body>
+       <?php
+
+        echo $data['info_en']; 
+        ?>
+    </body>
+</html>
