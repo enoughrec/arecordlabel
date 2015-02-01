537b8b83ab66fcfb8d4c963f0cc4076a48074e00
Dan Peddle <dan.peddle@gmail.com>
2015-02-01 17:42:55 +0100

    more info in desc and title

--- robot.php
+++ robot.php
@@ -3,8 +3,8 @@
     <head>
         <title><?php echo $data['album'].' - '.$data['artist'] ?></title>
         <meta name="description" content="<?php echo $data['cat'].' - '.$data['info_en'] ?>" />
-        <meta property="og:title" content="<?php echo $data['album'] ?>" />
-        <meta property="og:description" content="<?php echo $data['info_en'] ?>" />
+        <meta property="og:title" content="<?php echo $data['album'].' - '.$data['artist'] ?>" />
+        <meta property="og:description" content="<?php echo $data['cat'].' - '.$data['info_en'] ?>" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="<?php echo $data['url'] ?>" />
         <meta property="og:image" content="<?php echo $data['cover'] ?>" />
